bb1e37f5af05af5cf4f73d1adc3c0a2601c29ddd
Victor Castro <victorcastro.io@icloud.com>
2024-04-28 20:27:45 -0500

    Initial Commit

---
+++ MapachePedroPe Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapachePedroPe Watch App
+//
+//  Created by Victor Castro on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapachePedroPe Watch App/MapachePedroPeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapachePedroPeApp.swift
+//  MapachePedroPe Watch App
+//
+//  Created by Victor Castro on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MapachePedroPe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
